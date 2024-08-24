f9faaed2f4edb953d9b2b277576f78a3b5f92b31
Rasool16 <96374307+Rasool16@users.noreply.github.com>
2024-08-24 15:49:00 -0500

    Add files via upload

---
+++ Simple_project.py
@@ -0,0 +1,15 @@
+i = int(input("num1:"))
+j = int(input("num2:"))
+add = i + j
+sub = i - j
+mul = i * j
+select = input("Select input: ")
+if select == "add":
+    print(add)
+if select == "sun":
+    print(sub)
+if select == "mul":
+    print(mul)
+
+   
+ 
